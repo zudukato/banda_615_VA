994d00c64560b8273be9e69ff2df2c6ce59a33f7
Juan Alberto <Aplicaciones@revuelta.com.mx>
2024-01-10 14:43:49 -0600

    modificando documentacion de brmSql

--- Application.lua
+++ Application.lua
@@ -9,81 +9,105 @@ Description:
 *******************************************************************************
 ]]
 --Reqs
-require("awtxReqConstants")
-BrmReqSql = require("brmSql")
-local brmUtilitis = require("brmUtilitis")
+AwtxConstants = require("Reqs.awtxReqConstants")
+AwtxVariables = require("Reqs.awtxReqVariables")
+BrmReqSql = require("Reqs.brmSql")
+BrmUtilitis = require("Reqs.brmUtilitis")
+require("Reqs.awtxReqScaleKeys")
 
 
 --------------Setup------------------------
---Databases
--- Databases = {}
--- Databases.pesadas = BrmReqSql:new("pesadas")
--- Databases.catalogos = BrmReqSql:new("catalogos")
--- --tables
--- Databases.catalogos:newTable("Taras",{"Id VARCHAR NOT NULL","Peso INTEGER"})
--- Databases.catalogos:newTable("Empresas",{"Id VARCHAR NOT NULL", "Empresa VARCHAR NOT NULL"})
--- Databases.catalogos:newTable("Productos",{"Id VARCHAR NOT NULL", "producto VARCHAR NOT NULL"})
--- Databases.pesadas:newTable("firstWeight", { "Folio INTEGER",
---     "Placas VARCHAR NOT NULL",
---     "PesoIn INTEGER NOT NULL",
---     "UnitsIn VARCHAR NOT NULL",
---     "FechaIn DATE NOT NULL",
---     "HoraIn TIME NOT NULL",
---     "IdEmpresa VARCHAR",
---     "Empresa VARCHAR",
---     "IdProducto VARCHAR",
---     "Producto VARCHAR" })
+-- Databases
+Databases = {}
+Databases.pesadas = BrmReqSql:new("pesadas")
+Databases.catalogos = BrmReqSql:new("catalogos")
+--tables
+Databases.catalogos.taras = Databases.catalogos:newTable("taras", {"Id VARCHAR NOT NULL", "Peso INTEGER"})
+Databases.catalogos.empresas = Databases.catalogos:newTable("empresas", {"Id VARCHAR NOT NULL", "Empresa VARCHAR NOT NULL"})
+Databases.catalogos.productos = Databases.catalogos:newTable("productos", {"Id VARCHAR NOT NULL", "producto VARCHAR NOT NULL"})
+Databases.pesadas.primeraPesada = Databases.pesadas:newTable("primeraPesada", {"Folio INTEGER",
+    "Placas VARCHAR NOT NULL",
+    "PesoIn INTEGER NOT NULL",
+    "UnitsIn VARCHAR NOT NULL",
+    "FechaIn DATE NOT NULL",
+    "HoraIn TIME NOT NULL",
+    "IdEmpresa VARCHAR",
+    "Empresa VARCHAR",
+    "IdProducto VARCHAR",
+    "Producto VARCHAR"})
 
--- Databases.pesadas:newTable("secondWeight",{"Folio INTEGER",
---     "Placas VARCHAR NOT NULL",
---     "Chofer VARCHAR",
---     "PesoIn INTEGER NOT NULL",
---     "UnitsIn VARCHAR NOT NULL" ,
---     "FechaIn DATE NOT NULL",
---     "HoraIn TIME NOT NULL",
---     "PesoOut INTEGER NOT NULL",
---     "UnitsOut VARCHAR NOT NULL",
---     "FechaOut DATE NOT NULL",
---     "HoraOut TIME NOT NULL",
---     "Bruto INTEGER NOT NULL",
---     "Tara INTEGER NOT NULL",
---     "Neto INTEGER NOT NULL",
---     "IdEmpresa VARCHAR",
---     "Empresa VARCHAR",
---     "IdProducto VARCHAR",
---     "Producto VARCHAR" })
+Databases.pesadas.segundaPesada = Databases.pesadas:newTable("segundaPesada", {"Folio INTEGER",
+    "Placas VARCHAR NOT NULL",
+    "Chofer VARCHAR",
+    "PesoIn INTEGER NOT NULL",
+    "UnitsIn VARCHAR NOT NULL",
+    "FechaIn DATE NOT NULL",
+    "HoraIn TIME NOT NULL",
+    "PesoOut INTEGER NOT NULL",
+    "UnitsOut VARCHAR NOT NULL",
+    "FechaOut DATE NOT NULL",
+    "HoraOut TIME NOT NULL",
+    "Bruto INTEGER NOT NULL",
+    "Tara INTEGER NOT NULL",
+    "Neto INTEGER NOT NULL",
+    "IdEmpresa VARCHAR",
+    "Empresa VARCHAR",
+    "IdProducto VARCHAR",
+    "Producto VARCHAR"})
 
---  Databases.pesadas:newTable("firstFullWeight", { "Folio INTEGER",
---     "Placas1 VARCHAR NOT NULL",
---     "Placas2 VARCHAR NOT NULL",
---     "PesoIn1 INTEGER NOT NULL",
---     "PesoIn2 INTEGER NOT NULL",
---     "UnitsIn VARCHAR NOT NULL",
---     "FechaIn DATE NOT NULL",
---     "HoraIn TIME NOT NULL",
---     "IdEmpresa VARCHAR",
---     "Empresa VARCHAR",
---     "IdProducto VARCHAR",
---     "Producto VARCHAR" })
+Databases.pesadas.primeraPesadaFulles = Databases.pesadas:newTable("primeraPesadaFulles", {"Folio INTEGER",
+    "Placas1 VARCHAR NOT NULL",
+    "Placas2 VARCHAR NOT NULL",
+    "PesoIn1 INTEGER NOT NULL",
+    "PesoIn2 INTEGER NOT NULL",
+    "UnitsIn VARCHAR NOT NULL",
+    "FechaIn DATE NOT NULL",
+    "HoraIn TIME NOT NULL",
+    "IdEmpresa VARCHAR",
+    "Empresa VARCHAR",
+    "IdProducto VARCHAR",
+    "Producto VARCHAR"})
 
--- Databases.pesadas:newTable("secondFullWeight",{"Folio INTEGER",
---     "Placas1 VARCHAR NOT NULL",
---     "Placas2 VARCHAR NOT NULL",
---     "Chofer VARCHAR",
---     "PesoIn1 INTEGER NOT NULL",
---     "PesoIn2 INTEGER NOT NULL",
---     "UnitsIn VARCHAR NOT NULL" ,
---     "FechaIn DATE NOT NULL",
---     "HoraIn TIME NOT NULL",
---     "PesoOut1 INTEGER NOT NULL",
---     "PesoOut2 INTEGER NOT NULL",
---     "UnitsOut VARCHAR NOT NULL",
---     "FechaOut DATE NOT NULL",
---     "HoraOut TIME NOT NULL",
---     "Bruto INTEGER NOT NULL",
---     "Tara INTEGER NOT NULL",
---     "Neto INTEGER NOT NULL",
---     "IdEmpresa VARCHAR",
---     "Empresa VARCHAR",
---     "IdProducto VARCHAR",
---     "Producto VARCHAR"})
+Databases.pesadas.segundaPesadaFulles = Databases.pesadas:newTable("segundaPesadaFulles", {"Folio INTEGER",
+    "Placas1 VARCHAR NOT NULL",
+    "Placas2 VARCHAR NOT NULL",
+    "Chofer VARCHAR",
+    "PesoIn1 INTEGER NOT NULL",
+    "PesoIn2 INTEGER NOT NULL",
+    "UnitsIn VARCHAR NOT NULL",
+    "FechaIn DATE NOT NULL",
+    "HoraIn TIME NOT NULL",
+    "PesoOut1 INTEGER NOT NULL",
+    "PesoOut2 INTEGER NOT NULL",
+    "UnitsOut VARCHAR NOT NULL",
+    "FechaOut DATE NOT NULL",
+    "HoraOut TIME NOT NULL",
+    "Bruto INTEGER NOT NULL",
+    "Tara INTEGER NOT NULL",
+    "Neto INTEGER NOT NULL",
+    "IdEmpresa VARCHAR",
+    "Empresa VARCHAR",
+    "IdProducto VARCHAR",
+    "Producto VARCHAR"})
+------------------- Persistent variables for normal aplication -------------------
+--default values
+PresistentVars = PresistentVars or {}
+PresistentVars.currentMode = {value = "main"}--provitional
+PresistentVars.minWt = {value = 200}--min weight
+PresistentVars.userPassword = {value = 1793}--user Pasword
+PresistentVars.headers = {value = {"header1", "header2"}}-- ticket headers
+PresistentVars.ticketNumber = {value = 0}--ticket number
+PresistentVars.zeroThreashold = {value = 100}
+PresistentVars.zeroTareClear = {value = false}
+CurrentMode = require("operationModes.normalWeight.main")
+
+local function onStart()
+    -- persistent value asignation recalling last saved value
+    -- to acces "persistentVariable.nameOfVariable.value"
+    for name, defaultValue in pairs(PresistentVars) do
+        PresistentVars[name] = AwtxVariables.SavedVariable(name, defaultValue, true)
+    end
+
+end
+
+onStart()
--- Reqs/brmSql.lua
+++ Reqs/brmSql.lua
@@ -1,104 +1,120 @@
+---@class brmDatabase
 local brmDatabase = {}
-local dbtable = {}
 brmDatabase.__index = brmDatabase
+
+---@class dbtable
+local dbtable = {}
 dbtable.__index = dbtable
---Metodos de la clase brmDatabase, clase para controlar una base de datos del tipo SQLite
------------------------------------------------------------------------------------------
 
--- Constructor que crea una nueva instancia de una base de datos
--- databaseName :  str --nombre de la base de datos
--- -> instace : table --instancia
+--Constructor of the class brmDatabase
+---@param databaseName string
+---@return brmDatabase
+------------------------------------------------------------------------------------------
 function brmDatabase:new(databaseName)
-  local instance = setmetatable({}, brmDatabase)
-  instance.path = "C:\\Apps\\Database\\"
-  awtx.os.makeDirectory(instance.path)
-  instance.databaseName = (databaseName or "Revuelta") .. ".db"
-  instance:open()
-  return instance
+  self = setmetatable({}, brmDatabase)
+  self.path = "C:\\Apps\\Database\\"
+  awtx.os.makeDirectory(self.path)
+  self.databaseName = (databaseName or "Revuelta") .. ".db"
+  self:open()
+  return self
 end
 
--- Abre la comunicacion entre el objeto y la base de datos
+--Open database comunication
+------------------------------------------------------------------------------------------
 function brmDatabase:open()
-  local errors = {}
-  self.dbHandle, errors["opening"]= sqlite3.open(self.path .. self.databaseName)
-  print(errors)
+  local executionResult = {}
+  self.dbHandle, executionResult["opening"]= sqlite3.open(self.path .. self.databaseName)
+  print(executionResult)
 end
 
---Cierra la comunicacion entre el objeto y la base de datos
+--Close database comunication
+------------------------------------------------------------------------------------------
 function brmDatabase:close()
-  local errors = {}
-  errors["closing"] = self.dbHandle:close()
-  print(errors)
+  local executionResult = {}
+  executionResult["closing"] = self.dbHandle:close()
+  print(executionResult)
 end
 
---Metodo para crear una nueva Tabla
--- tableName : str  --Nombre de la tabla
--- fields: table --Inicializacion de los campos ({"Id INTEGER NOT NULL", Name TEXT})
--- -> errors: table --tabla con el resultado de las ejecuciones
-function brmDatabase:newTable(tableName, fields)
-  if not self.dbHandle:isopen() then print("Database is closed"); return end
-  if not tableName or not fields then print("tableName = nil or fields = nil") return end
+--Method to create a new database Tabla
+---@param tableName string --Name of the table
+---@param fieldsDefinition table --Initialization of fields ({"Id INTEGER NOT NULL", Name TEXT})
+---@return dbtable|nil -- instance of class dbtable
+------------------------------------------------------------------------------------------
+function brmDatabase:newTable(tableName, fieldsDefinition)
+  if not self.dbHandle:isopen() then print("Database is closed") return end
+  if type(tableName) ~="string" or type(fieldsDefinition) ~= "table" then print("tableName = nil or fields = nil") return end
   tableName = tostring(tableName)
-  local query = "CREATE TABLE IF NOT EXISTS " ..
-  tableName ..
-  " (" .. table.concat(fields, ",") .. ")"
-  local errors = {}
-  errors["beginTransaction"] = self.dbHandle:execute("BEGIN TRANSACTION")
-  errors["query"] = self.dbHandle:execute(query)
-  errors["commit"] = self.dbHandle:execute("COMMIT")
-  self[tableName] = dbtable:new(tableName, fields, self.dbHandle)
-  print(errors)
-  return errors
+  ---@type dbtable
+  return dbtable:new(tableName, fieldsDefinition, self.dbHandle)
 end
 
--- Metodos de la clase  dbtable, la cual es un manejador de las tablas de la base de datos
+--Handler for database tables
 ------------------------------------------------------------------------------------------
 
---Constructor de la calase
--- tableName: str --nombre de la tabla
--- fields: table|lsit --campos que contiene la tabla
--- dbHandle: object --manejador de la base de datos
+--Constructor
+---@param tableName string --name of the table
+---@param fieldsDefinition table --Initialization of fields ({"Id INTEGER NOT NULL", Name TEXT})
+---@param dbHandle table --handle of database
+---@return dbtable|nil --Instance of class
+------------------------------------------------------------------------------------------
 function dbtable:new(tableName, fieldsDefinition, dbHandle)
   if not dbHandle then return end
-  local instance = setmetatable({}, dbtable)
-  instance.tableName = tableName
-  instance._fieldsDefinition = fieldsDefinition
-  instance._headers = {}
+  self = setmetatable({}, dbtable)
+  self.tableName = tableName
+  self._fieldsDefinition = fieldsDefinition
+  self._headers = {}
   for _, value in ipairs(fieldsDefinition) do
-    table.insert(instance._headers, string.match(value, "%s*(%w+)%s*.?"))
+    table.insert(self._headers, string.match(value, "%s*(%w+)%s*.?"))
   end
-  instance._dbHandle = dbHandle
-  return instance
+  self._dbHandle = dbHandle
+
+  local query = "CREATE TABLE IF NOT EXISTS " ..
+  self.tableName ..
+  " (" .. table.concat(fieldsDefinition, ",") .. ")"
+
+  local executionResult = {
+    beginTransaction = self._dbHandle:execute("BEGIN TRANSACTION"),
+    query = self._dbHandle:execute(query),
+    commit = self._dbHandle:execute("COMMIT")
+  }
+  print(executionResult)
+  return self
 end
 
 -- Inserta un nuevo renglon a la base de datos
--- row: table --campos con valores
+---@param rowValues table --a table whit values 
+---@return table,table --response and result
+------------------------------------------------------------------------------------------
 function dbtable:addRow(rowValues)
   if not rowValues then print("Not Values") return  end
   if type(rowValues) ~= "table" then print("rowValues not table") end
   if #rowValues ~= #self._headers then print("error number of values") return end
 
   local query = "REPLACE INTO " .. self.tableName .. " VALUES(?" .. string.rep(',?', #rowValues - 1) .. ")"
-  local response, errors = self:_exec(query, unpack(rowValues))
-  print(errors)
-  return response, errors
+  local response, executionResult = self:_exec(query, unpack(rowValues))
+  print(executionResult)
+  return response, executionResult
 end
 
--- Metodo para eliminar un renglon de la base de datos
--- whereField: str -- campo por el cual se quiere buscar
--- whereValue: str\numeric -- valor del campo a buscar
+-- Method to delete a table row
+---@param whereField string -- field to find
+---@param whereValue any -- value of field
+---@return table|nil,table|nil --response and result
+------------------------------------------------------------------------------------------
 function dbtable:deleteRow(whereField, whereValue)
   local whereSentence, whereValue = self:_where(whereField, whereValue)
-  if whereSentence == "" then print("Who Row?") end
+  if whereSentence == "" then print("Who Row?") return end
   local query = "DELETE FROM " .. self.tableName .. whereSentence
-  local response,errors =self:_exec(query, whereValue)
-  print(errors)
-  return response, errors
+  local response,executionResult =self:_exec(query, whereValue)
+  print(executionResult)
+  return response, executionResult
 end
 
--- Metodo para editar un renglon
--- whereField: str -- campo por el cual se quiere buscar
--- whereValue: str\numeric -- valor del campo a buscar
+-- Method to change a database row
+---@param whereField string -- field to find
+---@param whereValue any -- value of field
+---@return table|nil,table|nil --response and result
+------------------------------------------------------------------------------------------
 function dbtable:updateRow(fields, fieldsValues, whereField, whereValue)
   local whereSentence, whereValue = self:_where(whereField, whereValue)
   fields= fields or self._headers
@@ -110,69 +126,78 @@ function dbtable:updateRow(fields, fieldsValues, whereField, whereValue)
 
   table.insert(fieldsValues,whereValue)
   local query = "UPDATE " .. self.tableName .. " SET " .. table.concat(fields,'=?,') .. "=? "..whereSentence
-  local response, errors = self:_exec(query,unpack(fieldsValues))
-  print(errors)
-  return response, errors
+  local response, executionResult = self:_exec(query,unpack(fieldsValues))
+  print(executionResult)
+  return response, executionResult
 end
 
--- Metodo para buscar renglones
--- whereField(opcional): str -- campo por el cual se quiere buscar
--- whereValue(opcional): str\numeric -- valor del campo a buscar
--- orderBy(opcional): str -- campo por el cual se quere ordenar
--- asc(opcional): bool -- si se quere ordenar ascendente o decendente
+-- Method to find rows
+---@param whereField? string -- field to find
+---@param whereValue? any -- value of field
+---@param orderBy? string --field to order
+---@param asc? boolean -- if order ascending or decending
+---@return table,table  --response and result of execution
+------------------------------------------------------------------------------------------
 function dbtable:find(whereField, whereValue, orderBy, asc)
   local whereSentence, whereValue = self:_where(whereField, whereValue)
   local orderBySentence = self:_orderBy(orderBy, asc)
   local query = "SELECT * FROM " .. self.tableName .. whereSentence .. orderBySentence
-  local response, errors = self:_exec(query, whereValue)
-  print(errors)
-  return response
+  local response, executionResult = self:_exec(query, whereValue)
+  print(executionResult)
+  return response,executionResult
 end
 
--- Metodo para contar el resultado de una busqueda
--- whereField(opcional): str -- campo por el cual se quiere buscar
--- whereValue(opcional): str\numeric -- valor del campo a buscar
+-- Count the result 
+---@param whereField? string -- field to find
+---@param whereValue? any -- value of field
+---@return integer,table
+------------------------------------------------------------------------------------------
 function dbtable:countRecord(whereField, whereValue)
   local whereSentence, whereValue = self:_where(whereField, whereValue)
   local query = "SELECT COUNT(*) FROM " .. self.tableName .. whereSentence
-  local response, errors = self:_exec(query, whereValue)
-  print(errors)
-  return response[1]["COUNT(*)"]
+  local response, executionResult = self:_exec(query, whereValue)
+  print(executionResult)
+  return response[1]["COUNT(*)"],executionResult
 end
 
--- Metodo privado para realizar la sentencia de ordenar
--- orderBy(opcional): str -- campo por el cual se quere ordenar
--- asc(opcional): bool -- si se quere ordenar ascendente o decendente
+--Private method to create a sentence to order a execution
+---@param orderBy? string --field to order
+---@param asc? boolean -- if order ascending or decending
+---@return string --sentence
+------------------------------------------------------------------------------------------
 function dbtable:_orderBy(orderBy, asc)
   if not orderBy then return "" end
   if type(orderBy) ~= "string" then print("orderBy("..orderBy..") Not String") end
-  asc = asc and "ASC" or "DESC"
-  return " ORDER BY " .. orderBy .. " " .. asc
+  local ascString = asc and "ASC" or "DESC"
+  return " ORDER BY " .. orderBy .. " " .. ascString
 end
 
--- Metodo privado para realizar la sentencia  al buscar
--- whereField(opcional): str -- campo por el cual se quiere buscar
--- whereValue(opcional): str\numeric -- valor del campo a buscar
+-- Private method to create a sentece to find 
+---@param whereField? string -- field to find
+---@param whereValue? any -- value of field
+---@return string,any --sentence
+------------------------------------------------------------------------------------------
 function dbtable:_where(whereField, whereValue)
   if not whereField or not whereValue then return "" end
   if type(whereField) ~= 'string' then print("whereField ("..whereField..") Not String") return "" end
   return " WHERE " .. whereField .. " = ?", whereValue
 end
 
--- Metodo privado para realizar la ejecucion de una query
--- query: str -- instruccion para SQLite3
+-- Private method to execut a query
+---@param query string --Query to execut 
+---@return table,table --response and result
+------------------------------------------------------------------------------------------
 function dbtable:_exec(query, ...)
   local response = {}
-  local errors   = {query = query, values = {...}}
+  local executionResult   = {query = query, values = {...}}
   local stmt     = nil
   if not query or type(query) ~= "string" then return {} , {query = "not string ("..query..")"} end
   if not self._dbHandle:isopen() then return response, { ["open"] = "Database is closed" } end
-  stmt, errors["queryExec"] = self._dbHandle:prepare(query)
-  if not stmt then return {}, errors end
-  if ... then errors['valuesExec'] = stmt:bind_values(...) end
+  stmt, executionResult["queryExec"] = self._dbHandle:prepare(query)
+  if not stmt then return {}, executionResult end
+  if ... then executionResult['valuesExec'] = stmt:bind_values(...) end
   for row in stmt:nrows() do table.insert(response, row) end
-
-  return response, errors
+  return response, executionResult
 end
 
 return brmDatabase
