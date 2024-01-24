24de95476b0a1b706c4693f05b9cc1dd9f5cbdb6
Juan Alberto <Aplicaciones@revuelta.com.mx>
2024-01-23 18:42:51 -0600

    efficient program returning functions

--- Reqs/awtxReqScaleKeys.lua
+++ Reqs/awtxReqScaleKeys.lua
@@ -14,9 +14,9 @@ The default handled event keys may or may not exist on other hardware.
 Functions:
 -- These create the default functionality for the keys.
 -- Redefine the function to change the default functionality.
-  if CurrentMode.keypad.onXXXKeyDown then CurrentMode.keypad.onXXXKeyDown() end
-  if CurrentMode.keypad.onXXXKeyHold then CurrentMode.keypad.onXXXKeyHold() end
-  if CurrentMode.keypad.onXXXKeyUp then CurrentMode.keypad.onXXXKeyUp() end
+  if CurrentMode.keypad.onXXXKeyDown then return CurrentMode.keypad.onXXXKeyDown() end
+  if CurrentMode.keypad.onXXXKeyHold then return CurrentMode.keypad.onXXXKeyHold() end
+  if CurrentMode.keypad.onXXXKeyUp then return CurrentMode.keypad.onXXXKeyUp() end
   
 Event Handlers:
 -- Leave these as-is as they create your hold events.
@@ -60,19 +60,19 @@ local zeroHoldFlag = 0
 ---------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_TARE_DOWN()
   tareHoldFlag = 0
-  if CurrentMode.keypad.onTareKeyDown then CurrentMode.keypad.onTareKeyDown() end
+  if CurrentMode.keypad.onTareKeyDown then return CurrentMode.keypad.onTareKeyDown() end
 end
 
 function awtx.keypad.KEY_TARE_REPEAT()
   tareHoldFlag = tareHoldFlag + 1
   if tareHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onTareKeyHold then CurrentMode.keypad.onTareKeyHold() end
+    if CurrentMode.keypad.onTareKeyHold then return CurrentMode.keypad.onTareKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_TARE_UP()
   if tareHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onTareKeyUp then CurrentMode.keypad.onTareKeyUp() end
+    if CurrentMode.keypad.onTareKeyUp then return CurrentMode.keypad.onTareKeyUp() end
   end
   tareHoldFlag = 0
 end
@@ -83,19 +83,20 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_SELECT_DOWN()
   selectHoldFlag = 0
-  if CurrentMode.keypad.onSelectKeyDown then CurrentMode.keypad.onSelectKeyDown() end
+  if CurrentMode.keypad.onSelectKeyDown then return CurrentMode.keypad.onSelectKeyDown() end
+   awtx.weight.cycleActiveValue()
  end
 
 function awtx.keypad.KEY_SELECT_REPEAT()
   selectHoldFlag = selectHoldFlag + 1
   if selectHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onSelectKeyHold then CurrentMode.keypad.onSelectKeyHold() end
+    if CurrentMode.keypad.onSelectKeyHold then return CurrentMode.keypad.onSelectKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_SELECT_UP()
   if selectHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onSelectKeyUp then CurrentMode.keypad.onSelectKeyUp() end
+    if CurrentMode.keypad.onSelectKeyUp then return CurrentMode.keypad.onSelectKeyUp() end
   end
   selectHoldFlag = 0
 end
@@ -106,19 +107,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_PRINT_DOWN()
   printHoldFlag = 0
-  if CurrentMode.keypad.onPrintKeyDown then CurrentMode.keypad.onPrintKeyDown() end
+  if CurrentMode.keypad.onPrintKeyDown then return CurrentMode.keypad.onPrintKeyDown() end
 end
 
 function awtx.keypad.KEY_PRINT_REPEAT()
   printHoldFlag = printHoldFlag + 1
   if printHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onPrintKeyHold then CurrentMode.keypad.onPrintKeyHold() end
+    if CurrentMode.keypad.onPrintKeyHold then return CurrentMode.keypad.onPrintKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_PRINT_UP()
   if printHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onPrintKeyUp then CurrentMode.keypad.onPrintKeyUp() end
+    if CurrentMode.keypad.onPrintKeyUp then return CurrentMode.keypad.onPrintKeyUp() end
   end
   printHoldFlag = 0
 end
@@ -129,19 +130,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_UNITS_DOWN()
   unitsHoldFlag = 0
-  if CurrentMode.keypad.onUnitsKeyDown then CurrentMode.keypad.onUnitsKeyDown() end
+  if CurrentMode.keypad.onUnitsKeyDown then return CurrentMode.keypad.onUnitsKeyDown() end
 end
 
 function awtx.keypad.KEY_UNITS_REPEAT()
   unitsHoldFlag = unitsHoldFlag + 1
   if unitsHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onUnitsKeyHold then CurrentMode.keypad.onUnitsKeyHold() end
+    if CurrentMode.keypad.onUnitsKeyHold then return CurrentMode.keypad.onUnitsKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_UNITS_UP()
   if unitsHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onUnitsKeyUp then CurrentMode.keypad.onUnitsKeyUp() end
+    if CurrentMode.keypad.onUnitsKeyUp then return CurrentMode.keypad.onUnitsKeyUp() end
   end
   unitsHoldFlag = 0
 end
@@ -152,19 +153,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_ZERO_DOWN()
   zeroHoldFlag = 0
-  if CurrentMode.keypad.onZeroKeyDown then CurrentMode.keypad.onZeroKeyDown() end
+  if CurrentMode.keypad.onZeroKeyDown then return CurrentMode.keypad.onZeroKeyDown() end
 end
 
 function awtx.keypad.KEY_ZERO_REPEAT()
   zeroHoldFlag = zeroHoldFlag + 1
   if zeroHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onZeroKeyHold then CurrentMode.keypad.onZeroKeyHold() end
+    if CurrentMode.keypad.onZeroKeyHold then return CurrentMode.keypad.onZeroKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_ZERO_UP()
   if zeroHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onZeroKeyUp then CurrentMode.keypad.onZeroKeyUp() end
+    if CurrentMode.keypad.onZeroKeyUp then return CurrentMode.keypad.onZeroKeyUp() end
   end
   zeroHoldFlag = 0
 end
@@ -175,19 +176,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_SAMPLE_DOWN()
   sampleHoldFlag = 0
-  if CurrentMode.keypad.onSampleKeyDown then CurrentMode.keypad.onSampleKeyDown() end
+  if CurrentMode.keypad.onSampleKeyDown then return CurrentMode.keypad.onSampleKeyDown() end
 end
 
 function awtx.keypad.KEY_SAMPLE_REPEAT()
   sampleHoldFlag = sampleHoldFlag + 1
   if sampleHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onSampleKeyHold then CurrentMode.keypad.onSampleKeyHold() end
+    if CurrentMode.keypad.onSampleKeyHold then return CurrentMode.keypad.onSampleKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_SAMPLE_UP()
   if sampleHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onSampleKeyUp then CurrentMode.keypad.onSampleKeyUp() end
+    if CurrentMode.keypad.onSampleKeyUp then return CurrentMode.keypad.onSampleKeyUp() end
   end
   sampleHoldFlag = 0
 end
@@ -198,19 +199,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_START_DOWN()
   startHoldFlag = 0
-  if CurrentMode.keypad.onStartKeyDown then CurrentMode.keypad.onStartKeyDown() end
+  if CurrentMode.keypad.onStartKeyDown then return CurrentMode.keypad.onStartKeyDown() end
 end
 
 function awtx.keypad.KEY_START_REPEAT()
   startHoldFlag = startHoldFlag + 1
   if startHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onStartKeyHold then CurrentMode.keypad.onStartKeyHold() end
+    if CurrentMode.keypad.onStartKeyHold then return CurrentMode.keypad.onStartKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_START_UP()
   if startHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onStartKeyUp then CurrentMode.keypad.onStartKeyUp() end
+    if CurrentMode.keypad.onStartKeyUp then return CurrentMode.keypad.onStartKeyUp() end
   end
   startHoldFlag = 0
 end
@@ -221,19 +222,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_STOP_DOWN()
   stopHoldFlag = 0
-  if CurrentMode.keypad.onStopKeyDown then CurrentMode.keypad.onStopKeyDown() end
+  if CurrentMode.keypad.onStopKeyDown then return CurrentMode.keypad.onStopKeyDown() end
 end
 
 function awtx.keypad.KEY_STOP_REPEAT()
   stopHoldFlag = stopHoldFlag + 1
   if stopHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onStopKeyHold then CurrentMode.keypad.onStopKeyHold() end
+    if CurrentMode.keypad.onStopKeyHold then return CurrentMode.keypad.onStopKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_STOP_UP()
   if stopHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onStopKeyUp then CurrentMode.keypad.onStopKeyUp() end
+    if CurrentMode.keypad.onStopKeyUp then return CurrentMode.keypad.onStopKeyUp() end
   end
   stopHoldFlag = 0
 end
@@ -244,19 +245,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_F1_DOWN()
   f1HoldFlag = 0
-  if CurrentMode.keypad.onF1KeyDown then CurrentMode.keypad.onF1KeyDown() end
+  if CurrentMode.keypad.onF1KeyDown then return CurrentMode.keypad.onF1KeyDown() end
 end
 
 function awtx.keypad.KEY_F1_REPEAT()
   f1HoldFlag = f1HoldFlag + 1
   if f1HoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onF1KeyHold then CurrentMode.keypad.onF1KeyHold() end
+    if CurrentMode.keypad.onF1KeyHold then return CurrentMode.keypad.onF1KeyHold() end
   end
 end
 
 function awtx.keypad.KEY_F1_UP()
   if f1HoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onF1KeyUp then CurrentMode.keypad.onF1KeyUp() end
+    if CurrentMode.keypad.onF1KeyUp then return CurrentMode.keypad.onF1KeyUp() end
   end
   f1HoldFlag = 0
 end
@@ -267,19 +268,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_SCALE_SELECT_DOWN()
   scaleSelectHoldFlag = 0
-   if CurrentMode.keypad.onScaleSelectKeyDown then CurrentMode.keypad.onScaleSelectKeyDown() end
+   if CurrentMode.keypad.onScaleSelectKeyDown then return CurrentMode.keypad.onScaleSelectKeyDown() end
 end
 
 function awtx.keypad.KEY_SCALE_SELECT_REPEAT()
   scaleSelectHoldFlag = scaleSelectHoldFlag + 1
   if scaleSelectHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onScaleSelectKeyHold then CurrentMode.keypad.onScaleSelectKeyHold() end
+    if CurrentMode.keypad.onScaleSelectKeyHold then return CurrentMode.keypad.onScaleSelectKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_SCALE_SELECT_UP()
   if scaleSelectHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onScaleSelectKeyUp then CurrentMode.keypad.onScaleSelectKeyUp() end
+    if CurrentMode.keypad.onScaleSelectKeyUp then return CurrentMode.keypad.onScaleSelectKeyUp() end
   end
   scaleSelectHoldFlag = 0
 end
@@ -290,19 +291,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_SETUP_DOWN()
   setupHoldFlag = 0
-  if CurrentMode.keypad.onSetupKeyDown then CurrentMode.keypad.onSetupKeyDown() end
+  if CurrentMode.keypad.onSetupKeyDown then return CurrentMode.keypad.onSetupKeyDown() end
 end
 
 function awtx.keypad.KEY_SETUP_REPEAT()
   setupHoldFlag = setupHoldFlag + 1
   if setupHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onSetupKeyHold then CurrentMode.keypad.onSetupKeyHold() end
+    if CurrentMode.keypad.onSetupKeyHold then return CurrentMode.keypad.onSetupKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_SETUP_UP()
   if setupHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onSetupKeyUp then CurrentMode.keypad.onSetupKeyUp() end
+    if CurrentMode.keypad.onSetupKeyUp then return CurrentMode.keypad.onSetupKeyUp() end
   end
   setupHoldFlag = 0
 end
@@ -313,19 +314,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_TARGET_DOWN()
   targetHoldFlag = 0
-  if CurrentMode.keypad.onTargetKeyDown then CurrentMode.keypad.onTargetKeyDown() end
+  if CurrentMode.keypad.onTargetKeyDown then return CurrentMode.keypad.onTargetKeyDown() end
 end
 
 function awtx.keypad.KEY_TARGET_REPEAT()
   targetHoldFlag = targetHoldFlag + 1
   if targetHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onTargetKeyHold then CurrentMode.keypad.onTargetKeyHold() end
+    if CurrentMode.keypad.onTargetKeyHold then return CurrentMode.keypad.onTargetKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_TARGET_UP()
   if targetHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onTargetKeyUp then CurrentMode.keypad.onTargetKeyUp() end
+    if CurrentMode.keypad.onTargetKeyUp then return CurrentMode.keypad.onTargetKeyUp() end
   end
   targetHoldFlag = 0
 end
@@ -359,19 +360,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_CLEAR_DOWN()
   clearHoldFlag = 0
-  if CurrentMode.keypad.onClearKeyDown then CurrentMode.keypad.onClearKeyDown() end
+  if CurrentMode.keypad.onClearKeyDown then return CurrentMode.keypad.onClearKeyDown() end
 end
 
 function awtx.keypad.KEY_CLEAR_REPEAT()
   clearHoldFlag = clearHoldFlag + 1
   if clearHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onClearKeyHold then CurrentMode.keypad.onClearKeyHold() end
+    if CurrentMode.keypad.onClearKeyHold then return CurrentMode.keypad.onClearKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_CLEAR_UP()
   if clearHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onClearKeyUp then CurrentMode.keypad.onClearKeyUp() end
+    if CurrentMode.keypad.onClearKeyUp then return CurrentMode.keypad.onClearKeyUp() end
   end
   clearHoldFlag = 0
 end
@@ -382,19 +383,19 @@ end
 -----------------------------------------------------------------------------------------------
 function awtx.keypad.KEY_DECIMAL_DOWN()
   decimalHoldFlag = 0
-  if CurrentMode.keypad.onDecimalKeyDown then CurrentMode.keypad.onDecimalKeyDown() end
+  if CurrentMode.keypad.onDecimalKeyDown then return CurrentMode.keypad.onDecimalKeyDown() end
 end
 
 function awtx.keypad.KEY_DECIMAL_REPEAT()
   decimalHoldFlag = decimalHoldFlag + 1
   if decimalHoldFlag == HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onDecimalKeyHold then CurrentMode.keypad.onDecimalKeyHold() end
+    if CurrentMode.keypad.onDecimalKeyHold then return CurrentMode.keypad.onDecimalKeyHold() end
   end
 end
 
 function awtx.keypad.KEY_DECIMAL_UP()
   if decimalHoldFlag < HowManyRepeatsMakeAHold then
-    if CurrentMode.keypad.onDecimalKeyUp then CurrentMode.keypad.onDecimalKeyUp() end
+    if CurrentMode.keypad.onDecimalKeyUp then return CurrentMode.keypad.onDecimalKeyUp() end
   end
   decimalHoldFlag = 0
 end
