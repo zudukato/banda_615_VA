ae1edc9a89e44792adf84306f26047c4c91581f8
Juan Alberto Hernandez Ramirez <juan/aplicaciones@revuelta.com.mx>
2025-11-17 09:06:59 -0600

    feat: actualizar importación de productos desde CSV
    - Cambia REPLACE INTO por estrategia DELETE + INSERT para reconstruir la tabla products
    - Ajusta formato de INSERT para 18 columnas según nuevo layout de CSV
    - Incrementa validación mínima de columnas (#row >= 18)
    - Mejora detección de archivos CSV en USB (búsqueda case-insensitive y manejo de “No USB”/“No File”)
    - Añade trazas de depuración (impresión de query y resultado de exec) durante la importación

--- operationModes/EPI/importProducts.lua
+++ operationModes/EPI/importProducts.lua
@@ -1,17 +1,17 @@
-local brmScreenRAD6015     = require("Reqs.brmScreenRAD6015")
-local database = require("operationModes.EPI.epiDatabase")
-local brmUtilities = require("Reqs.brmUtilities")
-local importProducts  = {keypad = {}}
-local exitFlag = true
+local brmScreenRAD6015 = require("Reqs.brmScreenRAD6015")
+local database         = require("operationModes.EPI.epiDatabase")
+local brmUtilities     = require("Reqs.brmUtilities")
+local importProducts   = { keypad = {} }
+local exitFlag         = true
 ---@type string[]
 
-importProducts.screen = brmScreenRAD6015.newScreen("importProducts")
+importProducts.screen  = brmScreenRAD6015.newScreen("importProducts")
 importProducts.screen:newLabel("title", "Importar Productos", { x = 0, y = 0 },
     { width = 400, height = 30 },
     0, 30, true, false)
-importProducts.screen:newLabel("info","" , { x = 0, y = 40 },
+importProducts.screen:newLabel("info", "", { x = 0, y = 40 },
     { width = 100, height = 30 },
-    0,31, true, false)
+    0, 31, true, false)
 importProducts.screen:newLabel("count", "", { x = 120, y = 110 }, { width = 39, height = 20 }, 5, 4, true, false)
 importProducts.screen:newLabel("total", "", { x = 160, y = 110 }, { width = 39, height = 20 }, 4, 4, true, false)
 importProducts.screen:newLabel("statusBar", "", { x = 0, y = 135 },
@@ -23,7 +23,7 @@ importProducts.screen:newButton("enter", "Enter", { x = 130, y = 148 }, { width 
 importProducts.screen:newButton("exit", "EXIT", { x = 258, y = 148 }, { width = 60, height = 20 }, 2, 4, true, false)
 local PrevMode
 
-importProducts.init = function (prevMode)
+importProducts.init = function(prevMode)
     PrevMode = prevMode
     importProducts.screen.labels.count:setText("")
     importProducts.screen.labels.total:setText("")
@@ -33,11 +33,13 @@ importProducts.init = function (prevMode)
 end
 
 
----This function should be changed; we should use a function in brmSqlClass, but it does not exist yet. 
-importProducts.import = function ()
+---This function should be changed; we should use a function in brmSqlClass, but it does not exist yet.
+importProducts.import = function()
     exitFlag = false
     local path = importProducts.getPath()
-    if not path then exitFlag = true; return end
+    if not path then
+        exitFlag = true; return
+    end
     importProducts.screen.labels.info:setText("ESPERE")
     local tr = table.csvToTable(path)
     importProducts.screen.labels.info:setText("")
@@ -48,15 +50,20 @@ importProducts.import = function ()
     importProducts.screen.labels.total:setVisible(true)
     importProducts.screen.labels.count:setVisible(true)
     dbHandle:exec("BEGIN TRANSACTION")
+    dbHandle:exec("DELETE FROM products;")
     for i, row in pairs(tr) do
-        if #row >= 11 then
+        if #row >= 18 then
             local result, query = pcall(function()
-                return string.format("REPLACE INTO products VALUES(%d,'%s','%s',%f,%f,%f,%f,%f,%f,%f,'%s')",
-                    tonumber(row[1]), row[2], row[3], tonumber(row[4]), tonumber(row[5]),
-                    tonumber(row[6]), tonumber(row[7]), tonumber(row[8]), tonumber(row[9]), tonumber(row[10]), row[11])
+                return string.format(
+                "INSERT INTO products VALUES(%s,%s,'%s','%s','%s','%s',%s,%s,%s,%s,%s,%s,'%s',%s, %s,'%s','%s','%s');",
+                    row[1], row[2], row[3], row[4], row[5],
+                    row[6], row[7], row[8], row[9], row[10], row[11],
+                    row[12], row[13], row[14], row[15],row[16],row[17],row[18])
             end)
             if result then
-                dbHandle:exec(query)
+                print(query)
+                local res, er =dbHandle:exec(query)
+                print(res, er)
             end
             importProducts.screen.labels.count:setText(("%d"):format(i))
             local prop = math.floor((i * 300) / total)
@@ -68,28 +75,28 @@ importProducts.import = function ()
     print("done")
 end
 
-importProducts.getPath = function ()
+importProducts.getPath = function()
     local path = awtx.os.amISimulator() and "c:\\Apps\\Reqs\\" or "g:\\"
     local pathList = awtx.os.getFiles(path)
     _, pathList = brmUtilities.keysValues(pathList)
-    if not type(pathList) then return brmUtilities.doScroll('No USB', 1000) end
+    if #pathList <=0 then return brmUtilities.doScroll('No USB', 1000) end
     local newPathList = {}
     local i = 1
     for _, path in pairs(pathList) do
-        if path:find(".csv") then table.insert(newPathList,path) end
+        if path:lower():match("%.csv$") then table.insert(newPathList, path) end
     end
     if #newPathList == 0 then return brmUtilities.doScroll("No File") end
-    local choice, isEnterKey = awtx.keypad.selectList(table.concat(newPathList,','), 1, -1, "select file")
+    local choice, isEnterKey = awtx.keypad.selectList(table.concat(newPathList, ','), 1, -1, "select file")
     if not isEnterKey then return end
-    return newPathList[choice+1]
+    return newPathList[choice + 1]
 end
 
-importProducts.keypad.onF3KeyUp =function ()
+importProducts.keypad.onF3KeyUp = function()
     if not exitFlag then return end
     importProducts.import()
-end 
+end
 importProducts.keypad.onF5KeyUp = function()
-    if not exitFlag  then return end
+    if not exitFlag then return end
     PrevMode.init()
 end
 return importProducts
