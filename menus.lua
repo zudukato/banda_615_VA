13aeba51350dfed0d4dae1948193586bcd5dc0de
Juan Alberto <Juanalbert.jh@gmail.com>
2025-11-20 08:06:50 -0600

    fix: ahora se puede modificar el zerothreshold por configuracion

--- menus.lua
+++ menus.lua
@@ -126,6 +126,12 @@ local menusTree =
     },
     config = {
         { text = Language._phrases.minWt, action = "FUNC", value = f.changeMinWt },
+        { text = "ZERO TH", action = "FUNC", value = function ()
+            local zero = awtx.keypad.enterFloat(PersistentVars.zeroThreshold,0,80000,-1,"Ingrese Zero")
+            if not zero then return end
+            PersistentVars.zeroThreshold = zero
+            Zero = zero
+        end },
     },
     reset = {
         { text = Language._phrases.resetAll, action = "FUNC", value = f.resetAll },
